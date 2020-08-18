6cee998e88cea4119b3bc4ca4390fb5db6ae8db2
Emmanuel Tesauro <emmanuel.tesauro@gmail.com>
2020-08-18 12:37:17 +0200

    First commit

---
+++ Shared/FindPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindPlayerApp.swift
+//  Shared
+//
+//  Created by Emmanuel Tesauro on 18/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct FindPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
